acd36fed5b06e4e409e74e89034ba64788f8be36
Gabriel Olivares <7gabe.o@gmail.com>
2023-07-12 09:08:32 -0700

    updating projects

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,7 +11,7 @@ const seo = {
   description:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   og: {
-    title: "GO",
+    title: "2-GO",
     type: "website",
     url: "https://2-go.github.io/Portfolio/",
   },
